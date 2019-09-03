b19ee55a41cd8b2003295e39d08e07f59dbc1b2a
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-09-03 22:30:48 +0300

    Football team - the  beginig

---
+++ L02.Encapsulation/Problems-Solutions/Football-Team-Generator/Core/Engine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Football_Team_Generator.Core
+{
+    public class Engine
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
