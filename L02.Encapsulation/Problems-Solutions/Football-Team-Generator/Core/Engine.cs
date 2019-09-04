d2b50a04148e356f69d7c848d65d596bd0149e9e
stanislavtz <stanislavtz@gmail.com>
2019-09-04 11:59:33 +0300

    Engine.cs update

--- L02.Encapsulation/Problems-Solutions/Football-Team-Generator/Core/Engine.cs
+++ L02.Encapsulation/Problems-Solutions/Football-Team-Generator/Core/Engine.cs
@@ -7,17 +7,50 @@ namespace Football_Team_Generator.Core
 {
     public class Engine
     {
+        private List<FootballTeam> teamList;
+
+        public Engine()
+        {
+            teamList = new List<FootballTeam>();
+        }
+
         public void Run()
         {
             string input = Console.ReadLine();
 
-            try
+            while (true)
             {
-                Stat stat = new Stat("asd", int.Parse(input));
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
+                string[] args = input
+                    .Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    if (args[0] == "Rating")
+                    {
+                        break;
+                    }
+                    else if (args[0] == "Team")
+                    {
+                        string teamName = args[1];
+                        var team = new FootballTeam(teamName);
+                        teamList.Add(team);
+                    }
+                    else if (args[0] == "Add")
+                    {
+                        string teamName = args[1];
+                        string playerName = args[2];
+                    }
+                    else if (args[0] == "Remove")
+                    {
+
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                input = Console.ReadLine();
             }
         }
     }
