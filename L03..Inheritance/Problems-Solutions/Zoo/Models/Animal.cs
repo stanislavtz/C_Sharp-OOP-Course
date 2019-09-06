d263774663f7d661a1f97b051dd397f2ef306f22
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-09-06 17:03:45 +0300

    Updated Animal.cs

--- L03..Inheritance/Problems-Solutions/Zoo/Models/Animal.cs
+++ L03..Inheritance/Problems-Solutions/Zoo/Models/Animal.cs
@@ -1,12 +1,28 @@
-﻿namespace Zoo
+﻿using System;
+
+namespace Zoo
 {
     public class Animal
     {
-        public string Name { get; set; }
+        private string name;
 
         public Animal(string name)
         {
             this.Name = name;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name lengh, must be at least 3 symbols!");
+                }
+
+                this.name = value;
+            }
         }
     }
 }
