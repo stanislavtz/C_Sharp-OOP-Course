7312ba7e0755ad998348a81999dfdf203b5abc5a
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-09-08 20:24:21 +0300

    "Animals" skeleton v0.1

--- L03..Inheritance/Problems-Solutions/Animals/StartUp.cs
+++ L03..Inheritance/Problems-Solutions/Animals/StartUp.cs
@@ -1,10 +1,14 @@
-﻿namespace Animals
+﻿using Animals.Core;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            var engine = new Engine();
+
+            engine.Run();
         }
     }
 }
--- L03..Inheritance/Problems-Solutions/Restaurant/Models/Food/MainDishes/Fish.cs
+++ L03..Inheritance/Problems-Solutions/Restaurant/Models/Food/MainDishes/Fish.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Restaurant.Models.Food.MainDishes
+﻿namespace Restaurant.Models.Food.MainDishes
 {
     public class Fish : MainDish
     {
