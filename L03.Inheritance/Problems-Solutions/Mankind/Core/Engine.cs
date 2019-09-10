e6bbc108f0aa9a8dacdae0889584a1874e5e0e6f
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-09-10 19:35:38 +0300

    Revert "ManKind v2.2"
    This reverts commit 6444994b2b8a4b57f53a7100baa4dd88d092bb1b.

--- L03.Inheritance/Problems-Solutions/Mankind/Core/Engine.cs
+++ L03.Inheritance/Problems-Solutions/Mankind/Core/Engine.cs
@@ -1,32 +1,10 @@
-﻿using Mankind.Models;
-using System;
-
-namespace Mankind.Core
+﻿namespace Mankind.Core
 {
     public class Engine
     {
         public void Run()
         {
-            try
-            {
-                string[] studentInfo = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
-                Human student = new Student(studentInfo[0], studentInfo[1], studentInfo[2]);
-
-                string[] workerInfo = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-                Human worker = new Worker(workerInfo[0], workerInfo[1], decimal.Parse(workerInfo[2]), double.Parse(workerInfo[3]));
-
-                Console.WriteLine(student);
-                Console.WriteLine();
-                Console.WriteLine(worker);
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
         }
     }
 }
--- L03.Inheritance/Problems-Solutions/Mankind/Models/Human.cs
+++ L03.Inheritance/Problems-Solutions/Mankind/Models/Human.cs
@@ -1,71 +1,32 @@
 ﻿using Mankind.Exceptions;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace Mankind.Models
 {
     public class Human
     {
-        private const int MIN_FIRST_NAME_LENGTH = 4;
-        private const int MIN_LAST_NAME_LENGTH = 3;
-
         private string firstName;
         private string lastName;
 
-        public Human(string firstName, string lastName)
+        public Human(string fn, string ln)
         {
-            this.FirstName = firstName;
-            this.LastName = lastName;
+
         }
 
         public string FirstName
         {
             get => this.firstName;
-            private set
+            set
             {
-                ValidateFirstLetter(value, nameof(this.firstName));
-                ValidateNameLength(value, MIN_FIRST_NAME_LENGTH, nameof(this.firstName));
+                if (!char.IsUpper(value[0]))
+                {
+                    throw new ArgumentException
+                        (string.Format(ExcetionsData.InvalidFistNameStartingLetterException), value.Split()[0]);
+                }
 
                 this.firstName = value;
-            }
-        }
-
-        public string LastName
-        {
-            get => this.lastName;
-            private set
-            {
-                ValidateFirstLetter(value, nameof(this.lastName));
-                ValidateNameLength(value, MIN_LAST_NAME_LENGTH, nameof(this.lastName));
-
-                this.lastName = value;
-            }
-        }
-
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.AppendLine($"First Name: {this.FirstName}");
-            sb.AppendLine($"Last Name: {this.LastName}");
-
-            return sb.ToString().TrimEnd();
-        }
-
-        private static void ValidateNameLength(string name, int length, string parameter)
-        {
-            if (name.Length < length)
-            {
-                throw new ArgumentException
-                    (string.Format(ExceptionsData.InvalidNameLength, parameter, length));
-            }
-        }
-
-        private static void ValidateFirstLetter(string name, string parameter)
-        {
-            if (!char.IsUpper(name[0]))
-            {
-                throw new ArgumentException
-                    (string.Format(ExceptionsData.InvalidFirstLetter, parameter));
             }
         }
     }
