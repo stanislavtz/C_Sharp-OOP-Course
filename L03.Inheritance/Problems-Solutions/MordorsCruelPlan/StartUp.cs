adfdc0e715f37f9df5c969df4513ba77a8e3f8ac
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-09-11 23:05:03 +0300

    Mordor Plans v1.0

---
+++ L03.Inheritance/Problems-Solutions/MordorsCruelPlan/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MordorsCruelPlan
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] foods = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int currentPoints = 0;
+            int totalPoints = 0;
+
+            foreach (var food in foods)
+            {
+                switch (food.ToLower())
+                {
+                    case "cram":
+                        currentPoints = 2;
+                        break;
+                    case "lembas":
+                        currentPoints = 3;
+                        break;
+                    case "apple":
+                        currentPoints = 1;
+                        break;
+                    case "melon":
+                        currentPoints = 1;
+                        break;
+                    case "honeycake":
+                        currentPoints = 5;
+                        break;
+                    case "mushrooms":
+                        currentPoints = -10;
+                        break;
+                    default:
+                        currentPoints = -1;
+                        break;
+                }
+
+                totalPoints += currentPoints;
+            }
+
+            string mood = string.Empty;
+
+            if (totalPoints < -5)
+            {
+                mood = "Angry";
+            }
+            else if (totalPoints <= 0)
+            {
+                mood = "Sad";
+            }
+            else if (totalPoints <= 15)
+            {
+                mood = "Happy";
+            }
+            else
+            {
+                mood = "JavaScript";
+            }
+
+            Console.WriteLine(totalPoints);
+            Console.WriteLine(mood);
+        }
+    }
+}
