9da8755dbe4c4f735071411bd5b13f92c282289c
stanislavtz <stanislavtz@gmail.com>
2019-09-13 14:38:25 +0300

    Person Info upload

---
+++ L04.Interfaces-And-Abstraction/Problems-Solutions/Person-Info/Person-Info/Citizen.cs
@@ -0,0 +1,27 @@
+﻿namespace PersonInfo
+{
+    using System.Text;
+
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(this.Name);
+            sb.AppendLine($"{this.Age}");
+
+            return sb.ToString().TrimEnd(); 
+        }
+    }
+}
