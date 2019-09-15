c0b471416f36ba6542df628508be770871ab1ec0
Stanislavtz Tzekov <stanislavtz@gmail.com>
2019-09-15 22:36:50 +0300

    Military elite v 1.2

---
+++ L04.Interfaces-And-Abstraction/Problems-Solutions/Military-Elite/Models/Soldier.cs
@@ -0,0 +1,17 @@
+﻿using Military_Elite.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Military_Elite.Models
+{
+    public abstract class Soldier : ISoldier
+    {
+
+        public string Id => throw new NotImplementedException();
+
+        public string FirstName => throw new NotImplementedException();
+
+        public string LastName => throw new NotImplementedException();
+    }
+}
