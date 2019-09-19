c3475025e59686def01a3fc6c3c653a6bdfe9471
Stanislavtz Tzekov <stanislavtz@gmail.com>
2019-09-19 08:42:45 +0300

    Military elite updated Engine class

--- L04.Interfaces-And-Abstraction/Problems-Solutions/Military-Elite/Core/Engine.cs
+++ L04.Interfaces-And-Abstraction/Problems-Solutions/Military-Elite/Core/Engine.cs
@@ -1,4 +1,5 @@
 ﻿using Military_Elite.Contracts;
+using Military_Elite.Contracts.SpecialForces;
 using Military_Elite.Models;
 using System;
 using System.Collections.Generic;
@@ -48,18 +49,58 @@ namespace Military_Elite.Core
                     {
                         decimal salary = decimal.Parse(args[4]);
 
-                        List<string> privatesCollection = args.Skip(5).ToList();
+                        string[] ids = args.Skip(5).ToArray();
 
                         ILieutenantGeneral soldier = new LieutenantGeneral(id, firstName, lastName, salary);
-                        
-                    }
 
-                   
+                        foreach (var item in ids)
+                        {
+                            var currentPrivate = privates.First(p => p.Id == item);
+                            soldier.AddSoldier(currentPrivate);
+                        }
+                    }
+                    else if (soldierType == "Engineer")
+                    {
+                        decimal salary = decimal.Parse(args[4]);
+                        string corps = args[5];
+
+                        string[] repairs = args.Skip(6).ToArray();
+
+                        IEngineer soldier = new Engineer(id, firstName, lastName, salary, corps);
+
+                        for (int i = 0; i < repairs.Length; i += 2)
+                        {
+                            string partName = repairs[i];
+                            int hoursWorked = int.Parse(repairs[i + 1]);
+
+                            IRepair currentRepair = new Repair(partName, hoursWorked);
+
+                            soldier.AddRepair(currentRepair);
+                        }
+                    }
+                    else if (soldierType == "Commando")
+                    {
+                        decimal salary = decimal.Parse(args[4]);
+                        string corps = args[5];
+
+                        string[] missions = args.Skip(6).ToArray();
+
+                        ICommando soldier = new Commando(id, firstName, lastName, salary, corps);
+
+                        for (int i = 0; i < missions.Length; i += 2)
+                        {
+                            string name = missions[i];
+                            string state = missions[i = 1];
+
+                            IMission currentMission = new Mission(name, state);
+
+                            soldier.AddMission(currentMission);
+                        }
+                    }
                 }
                 catch (Exception)
                 {
 
-                    throw;
                 }
 
                 input = Console.ReadLine();
