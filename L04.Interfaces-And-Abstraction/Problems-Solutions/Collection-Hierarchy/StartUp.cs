701204968b53e7fdb0cfc6663ca89f89d94cd34b
Stanislavtz Tzekov <stanislavtz@gmail.com>
2019-09-19 18:52:52 +0300

    Collection hierarchy initial version

---
+++ L04.Interfaces-And-Abstraction/Problems-Solutions/Collection-Hierarchy/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collection_Hierarchy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
