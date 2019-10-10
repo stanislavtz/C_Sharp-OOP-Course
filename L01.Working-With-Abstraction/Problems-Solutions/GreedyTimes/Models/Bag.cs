4ac279d72441524470f7df8b90e2f7a61a8a8f57
Stanislavtz Tzekov <stanislavtz@gmail.com>
2019-10-10 08:58:15 +0300

    Greedy times update

--- L01.Working-With-Abstraction/Problems-Solutions/GreedyTimes/Models/Bag.cs
+++ L01.Working-With-Abstraction/Problems-Solutions/GreedyTimes/Models/Bag.cs
@@ -14,5 +14,24 @@ namespace P05_GreedyTimes.Models
         }
 
         public int Capacity { get; private set; }
+
+        public IReadOnlyCollection<Gold> GoldCollection => this.goldCollection;
+        public IReadOnlyCollection<Gem> GemCollection => this.gemCollection;
+        public IReadOnlyCollection<Cash> CashCollection => this.cashCollection;
+
+        public void AddGold(IPrecious precious)
+        {
+
+        }
+
+        public void AddGem(IPrecious precious)
+        {
+
+        }
+
+        public void AddCash(IPrecious precious)
+        {
+
+        }
     }
 }
