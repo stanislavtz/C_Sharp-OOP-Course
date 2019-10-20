8df06d9a4c8aa1bfe167a597bacc2509e38bca29
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-10-20 23:19:18 +0300

    Reflection updated

--- L07.Reflection/Problems-Solutions/07. CSharp-OOP-Reflection-And-Attributes-Exercises-Resources/P01_HarvestingFields/HarvestingFieldsTest.cs
+++ L07.Reflection/Problems-Solutions/07. CSharp-OOP-Reflection-And-Attributes-Exercises-Resources/P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -13,9 +13,39 @@
                 .GetTypes()
                 .FirstOrDefault(x => x.Name == nameof(HarvestingFields));
 
-            var fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
 
+            string input = Console.ReadLine();
 
+            while (input != "HARVEST")
+            {
+                FieldInfo[] fieldInfosToPrint = null;
+
+                switch (input)
+                {
+                    case "private":
+                        fieldInfosToPrint = fieldInfos.Where(x => x.IsPrivate).ToArray();
+                        break;
+                    case "protected":
+                        fieldInfosToPrint = fieldInfos.Where(x => x.IsFamily).ToArray();
+                        break;
+                    case "public":
+                        fieldInfosToPrint = fieldInfos.Where(x => x.IsPublic).ToArray();
+                        break;
+                    default:
+                        fieldInfosToPrint = fieldInfos;
+                        break;
+                }
+
+                foreach (var field in fieldInfosToPrint)
+                {
+                    string accessModifier = field.Attributes.ToString().ToLower() == "family" ? "protected" : field.Attributes.ToString().ToLower();
+
+                    Console.WriteLine($"{accessModifier} {field.FieldType.Name.ToString()} {field.Name}");
+                }
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
