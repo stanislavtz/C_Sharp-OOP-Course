bcebf959a93e1ca3bf458557e0ee76cce73de909
Stanislavtz Tzekov <stanislavtz@students.softuni.bg>
2019-10-20 22:44:12 +0300

    Reflaction directories reorganized

---
+++ L07.Reflection/Problems-Solutions/07. CSharp-OOP-Reflection-And-Attributes-Exercises-Resources/P01_HarvestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_HarvestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            Type type = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .FirstOrDefault(x => x.Name == nameof(HarvestingFields));
+
+            var fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+
+
+        }
+    }
+}
