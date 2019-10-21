ea6930332e7e3cbaf68185eefd0d45b52ad69a76
stanislavtz <stanislavtz@gmail.com>
2019-10-21 15:09:17 +0300

    Reflection BlackBox initial version

--- L07.Reflection/Problems-Solutions/07. CSharp-OOP-Reflection-And-Attributes-Exercises-Resources/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
+++ L07.Reflection/Problems-Solutions/07. CSharp-OOP-Reflection-And-Attributes-Exercises-Resources/P02_BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace P02_BlackBoxInteger
 {
     using System;
+    using System.Linq;
     using System.Reflection;
 
     public class BlackBoxIntegerTests
@@ -9,7 +10,29 @@
         {
             Type type = typeof(BlackBoxInteger);
 
-            var Methods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            var instance = Activator.CreateInstance(type, true);
+
+            var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var inputArgs = input.Split("_");
+
+                var methodName = inputArgs[0];
+                var inputValue = int.Parse(inputArgs[1]);
+
+                var currentMethod = methods.FirstOrDefault(x => x.Name == methodName);
+
+                currentMethod.Invoke(instance, new object[] { inputValue });
+
+                var field = type.GetField("innerValue", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                Console.WriteLine(field.GetValue(instance));
+
+                input = Console.ReadLine();
+            }
         }
     }
 }
