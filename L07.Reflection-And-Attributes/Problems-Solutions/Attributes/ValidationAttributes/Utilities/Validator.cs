c41109a36ff778d58dbc022910677a7e3817df47
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-01-13 23:08:35 +0200

    Validation Attributes

---
+++ L07.Reflection-And-Attributes/Problems-Solutions/Attributes/ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] propInfos = obj
+                .GetType()
+                .GetProperties();
+
+            foreach (var property in propInfos)
+            {
+                var attributes = property
+                    .GetCustomAttributes()
+                    .Where(x => x is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                foreach (var attr in attributes)
+                {
+                    if (!attr.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
